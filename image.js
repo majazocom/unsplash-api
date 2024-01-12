cc82fd6616637a8fc9e848d10d26509d25b41ebd
majazocom <maja.gedda@zocom.se>
2024-01-12 13:51:02 +0100

    added some sass

--- image.js
+++ image.js
@@ -1,3 +1,10 @@
 const imageData = JSON.parse(localStorage.getItem("imageData"));
 console.log(imageData);
-document.getElementById("img").setAttribute("src", imageData.urls.full);
+document.getElementById("img").setAttribute("src", imageData.urls.thumb);
+document.getElementById("img").setAttribute("alt", imageData.alt_description);
+
+document.querySelector('.createdAt').innerText += imageData.user.name;
+// göra om datumet till YYYY-MM-DD
+let dateObj = new Date(imageData.created_at);
+document.querySelector('.uploadedAt').innerText += 
+dateObj.toLocaleDateString('se');
